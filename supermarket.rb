ed96dd13b55b70dd035b01e8b24781ae7a6661ba
diego-forero <43680183+diego-forero@users.noreply.github.com>
2018-10-11 02:30:55 -0500

    Update supermarket.rb

--- supermarket.rb
+++ supermarket.rb
@@ -1,64 +1,75 @@
-#settings es el array de instrucciones: settings[unique/multiple:simulationtype,:cashnumber,:simulationtime,:deltatime]
+class Supermarket
+  attr_reader :cashiers
 
-class supermarket
-
-  attr_accessor :elapsedTime :shorterLine
-  def initialize simulationtype:, cashnumber:, simulationtime:, deltatime: , elapsedtime:
-    @simulationype= simulation_type
-    @cashnumber=cash_number
-    @simulationtime=simulation_time
-    @deltatime=delta_time
+  def initialize number_of_cashiers:
+    create_cashiers number_of_cashiers
   end
 
-  def cashCreator
-    attr_accessor cash_number:
-    #@cashregister=Array.new
-    #crear el arreglo de cajas que contendra los arreglos de filas que se crearan el numero de veces solitiado(cash_number.times) lines.each
-    #hacer que: con el numero de cajas se cree un arreglo vacío que se vaya llenando con objetos de la clase Line (Line.new
-    totalLines=Array.new(cash_number){Line.new}
+  def create_cashiers number_of_cashiers
   end
 
-  def arrivalOfClients #este metodo seria de pasarlo a ambas clases hijo
-    elapsedTime = 0
-    while elapsedTime < simulationtime  do
-    if elapsedTime % 3 = 0
-      arrivedClients = Array.new(rand 5){Client.new}
-      arrivedClients.length.times.totalLines[shorterArrayIndex].receive_client arrivedClients # hacer (la longitud del arreglo arrivedClients) veces el metodo receive_client sobre la linea mas corta (totalLines[shorterArrayIndex]) con la variable arrived clients para que borre el cliente
-    end
-    elapsedTime +=1
-    end
-    # #usar modulo para que %3!=0 (cada 3 minutos) se cumpla el arrived clientes
-    # #@elapsedTime=
-    # #se crea un arreglo de x clientes nuevos donde x es un rand de 0 a 5
-    # arrivedClients = Array.new(rand 5){Client.new}
-
+  def receive_new_clients
+    Array.new(rand 5){Client.new}
   end
 
-  # private
-  #   def clientInsertion
-  #     #x<<y.delete_at(0)
-  #     shorterLine
-  #   end
+  def put_new_clients_in_queue
+  end
 
+  def work
+  end
 end
 
-class uniqueLine<supermarket
-  # shorterLine=totalLines[0]
-  shorterArrayIndex=0
+class Supermarket_unique_queue < Supermarket
+  attr_reader :cashiers
+
+  def create_cashiers number_of_cashiers
+    unique_queue = Queue.new
+    @cashiers =  Array.new(number_of_cashiers) {Cashier.new (unique_queue)}
+  end
+
+  def put_new_clients_in_queue
+    i = 0
+    new_clients = receive_new_clients
+    while i < new_clients.size  do
+      @cashiers[0].line.receive_client new_clients.shift
+    end
+  end
+
+  def work
+    @cashiers.each {|c| c.work}
+    @cashiers[0].line.count_wait_time_for_clients
+  end
 end
 
-class multipleLine<supermarket
+class Supermarket_multiple_queue < Supermarket
+  attr_reader :cashiers
 
-  def shorterLineAnalysis
-    initialize :shorterArrayIndex #se define variable para almacenar el index de la fila mas corta para ser usada en el clientInsertion
-    #analisis para tener la
-    #definir una variable shorterLine=0
-    shorterArray=totalLines[0]
-    totalLines.each_with_index do |comparisonLine, index|
-      if totalLines[index].number_of_clients<shorterArray.number_of_clients
-        shorterArray = comparisonLine
-        shorterArrayIndex = index
+  def create_cashiers number_of_cashiers
+    @cashiers =  Array.new(number_of_cashiers) {Cashier.new (Queue.new)}
+  end
+
+  def put_new_clients_in_queue
+    i = 0
+    new_clients = receive_new_clients
+    while i < new_clients.size  do
+      cashier_with_shortest_line.line.receive_client new_clients.shift
+    end
+  end
+
+  def cashier_with_shortest_line
+    temporal_cashier = @cashiers[0]
+    @cashiers.each do |c|
+      if temporal_cashier.line.number_of_clients > c.line.number_of_clients
+        temporal_cashier = c
       end
+    end
+    return temporal_cashier
+  end
+
+  def work
+    @cashiers.each do |c|
+      c.work
+      c.line.count_wait_time_for_clients
     end
   end
 
