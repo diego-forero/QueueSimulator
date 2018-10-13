ce37410a2bdd0d3d15f3141edc0b5876d021f1e4
diego-forero <43680183+diego-forero@users.noreply.github.com>
2018-10-12 19:43:29 -0500

    Update Supermarket.rb
    Se hace modificaciones sugeridas. Se aplica rubocop para corrección de estilo y se elimina el while de única línea.

--- supermarket.rb
+++ supermarket.rb
@@ -1,57 +1,58 @@
-class Supermarket
+load 'cashier.rb'
+load 'client.rb'
+load 'queue.rb'
+
+class Market
   attr_reader :cashiers
 
-  def initialize number_of_cashiers:
+  def initialize(number_of_cashiers:)
     create_cashiers number_of_cashiers
   end
 
-  def create_cashiers number_of_cashiers
-  end
-
   def receive_new_clients
-    Array.new(rand 5){Client.new}
-  end
-
-  def put_new_clients_in_queue
-  end
-
-  def work
+    Array.new(rand 5) { Client.new }
   end
 end
 
-class Supermarket_unique_queue < Supermarket
-  attr_reader :cashiers
+class MarketUniqueQueue < Market
+  attr_reader :cashiers, :queue
 
-  def create_cashiers number_of_cashiers
-    unique_queue = Queue.new
-    @cashiers =  Array.new(number_of_cashiers) {Cashier.new (unique_queue)}
+  def create_cashiers(number_of_cashiers)
+    @queue = Queue.new
+    @cashiers = Array.new(number_of_cashiers) { Cashier.new(@queue) }
   end
 
   def put_new_clients_in_queue
     i = 0
     new_clients = receive_new_clients
-    while i < new_clients.size  do
-      @cashiers[0].line.receive_client new_clients.shift
+    @queue.receive_client new_clients.shift while i < new_clients.size
+  end
+
+  def work
+    @cashiers.each(&:work)
+    @queue.count_wait_time_for_clients
+  end
+end
+
+class MarketMultipleQueue < Market
+  attr_reader :cashiers, :queues
+
+  def create_cashiers(number_of_cashiers)
+    @cashiers = []
+    @queues = []
+    i = 0
+    while i < number_of_cashiers
+      temp_queue = Queue.new
+      @queues << temp_queue
+      @cashiers << Cashier.new(temp_queue)
+      i += 1
     end
   end
 
-  def work
-    @cashiers.each {|c| c.work}
-    @cashiers[0].line.count_wait_time_for_clients
-  end
-end
-
-class Supermarket_multiple_queue < Supermarket
-  attr_reader :cashiers
-
-  def create_cashiers number_of_cashiers
-    @cashiers =  Array.new(number_of_cashiers) {Cashier.new (Queue.new)}
-  end
-
   def put_new_clients_in_queue
     i = 0
     new_clients = receive_new_clients
-    while i < new_clients.size  do
+    while i < new_clients.size
       cashier_with_shortest_line.line.receive_client new_clients.shift
     end
   end
@@ -63,7 +64,7 @@ class Supermarket_multiple_queue < Supermarket
         temporal_cashier = c
       end
     end
-    return temporal_cashier
+    temporal_cashier
   end
 
   def work
@@ -72,5 +73,4 @@ class Supermarket_multiple_queue < Supermarket
       c.line.count_wait_time_for_clients
     end
   end
-
 end
